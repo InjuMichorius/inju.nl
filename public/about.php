04d4d1752170611d57e660b921efb99292550040
inju <inju@allyourmedia.nl>
2022-03-17 16:53:05 +0100

    Changed map structure, finished twy styling

---
+++ public/about.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <?php include 'head.php'; ?>
+    <title>Inju Michorius | TWY</title>
+</head>
+
+<body>
+    <?php include 'header.php'; ?>
+    <main>
+		Hello my name is Inju Michorius
+    </main>
+</body>
+
+</html>
---
+++ public/head.php
@@ -0,0 +1,10 @@
+<meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="stylesheet" href="./styles/main.css">
+<link rel="stylesheet" href="./styles/header.css">
+<link rel="stylesheet" href="./styles/projects.css">
+<link rel="stylesheet" href="./styles/project.css">
+<link rel="stylesheet" href="./styles/cursor.css">
+<script src="./cursor.js" defer></script>
+<link rel="icon" type="png" href="../img/favicon16x16m.png">
