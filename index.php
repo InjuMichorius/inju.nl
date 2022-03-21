1458b9d1f4b41ed8d9ac924c98fd0e79f6f399f7
ÏnjuMichorius <Kato1234!>
2022-03-21 20:36:32 +0100

    Add hamburger menu and projects animation

--- index.php
+++ index.php
@@ -12,14 +12,19 @@
     <script src="/public/scripts/cursor.js" defer></script>
     <script src="/public/scripts/randomColor.js" defer></script>
     <link rel="icon" type="png" href="../img/favicon16x16m.png">
-    <title>Portfolio Inju Michorius</title>
+    <title>Portfolio | Inju Michorius</title>
 </head>
 
 <body>
     <?php include './public/header.php'; ?>
     <main>
-        <h1 class="banner_title">Inju Michorius is a Frontend Designer based in the Netherlands</h1>
-        <p class="banner_text">I’m a last year communication and Multimedia Design student, learning to become a front-end developer with UI/UX skills</p>
+        <svg id="Laag_1" data-name="Laag 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 528 416" class="logo">
+            <path class="path" stroke-width="4" d="M160,0V304.41A111.12,111.12,0,0,1,140,368.13a111.84,111.84,0,0,1-91.86,48.08h-.1v-320A48.17,48.17,0,0,0,0,48.07V0ZM319.86,0a111.79,111.79,0,0,0-111.8,111.8V416.21h.08A111.81,111.81,0,0,0,320,304.41V0ZM479.93,320V0h-.08A111.79,111.79,0,0,0,368,111.8V416.21H528V368.13A48.16,48.16,0,0,1,479.93,320Z" />
+        </svg>
+        <div class="wrapper">
+            <h1 class="banner_title">Inju Michorius is a Frontend Designer based in the Netherlands</h1>
+            <p class="banner_text">I’m a last year communication and Multimedia Design student, learning to become a front-end developer with UI/UX skills</p>
+        </div>
         <section class="section_projects">
             <article class="project">
                 <img class="project__img" src="/img/projectTWY.jpg" alt="">
--- public/head.php
+++ public/head.php
@@ -1,6 +1,7 @@
 <meta charset="UTF-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="icon" type="png" href="../img/favicon16x16m.png">
 <link rel="stylesheet" href="./styles/main.css">
 <link rel="stylesheet" href="./styles/header.css">
 <link rel="stylesheet" href="./styles/projects.css">
@@ -8,4 +9,3 @@
 <link rel="stylesheet" href="./styles/cursor.css">
 <script src="./scripts/cursor.js" defer></script>
 <script src="./scripts/randomColor.js"></script>
-<link rel="icon" type="png" href="../img/favicon16x16m.png">
